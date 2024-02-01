85db3601508f5e53b913fceb7c72ac750524d4b9
yousradiab <yousserah@gmail.com>
2024-02-01 15:15:37 +0100

    -

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,44 @@
 package org.example;
 
+import java.io.*;
+import java.net.URL;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        readURL(args[0],args[1]);
+        readURL(args[2],args[3]);
+    }
+public static void readURL(String url, String fileName) {
+
+    try {
+        //step 1
+        URL urlLink = new URL(url);
+        System.out.println(" URL: " + urlLink);
+
+        //Readers
+        InputStream stream = urlLink.openStream();
+        InputStreamReader inputReader = new InputStreamReader(stream);
+        BufferedReader reader = new BufferedReader(inputReader);
+
+        //Writers
+        FileWriter filewriter = new FileWriter(fileName);
+        BufferedWriter writer = new BufferedWriter(filewriter);
+        String inputLine = "";
+        while
+        (inputLine != null) {
+            //step 2
+            inputLine = reader.readLine();
+
+
+
+            writer.write(inputLine);
+        }
+    } catch (Exception e) {
     }
 }
+
+
+};
+
